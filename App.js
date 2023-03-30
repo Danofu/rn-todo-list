db95046f286d42f1b211508a1ff15a775e5ec431
Daniil Borysov <danofu13@gmail.com>
2023-03-30 19:26:36 +0200

    Create a simple goals list
    * add a user input to provide goal text
    * add button to add a goal
    * create a view to list all added goals

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (textValue) => {
+    setEnteredGoalText(textValue);
+  };
+
+  const addGoalHandler = () => {
+    setGoals((prevGoals) => [enteredGoalText, ...prevGoals]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          placeholder="Your course goal..."
+          style={styles.textInput}
+        />
+        <Button onPress={addGoalHandler} title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+        {goals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 15,
+    paddingTop: 50,
+  },
+  goalsContainer: {
+    flex: 6,
+  },
+  inputContainer: {
+    alignItems: "center",
+    borderBottomColor: "#CCC",
+    borderBottomWidth: 1,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 24,
+  },
+  textInput: {
+    borderColor: "#CCC",
+    borderWidth: 1,
+    marginRight: 8,
+    padding: 8,
+    width: "70%",
   },
 });
+
+export default App;
